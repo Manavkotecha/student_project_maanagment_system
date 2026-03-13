24e0db26bea1a50f05705fe5687da41dcfc51c2c
Manavkotecha <manavkotecha45@gmail.com>
2026-03-13 09:39:22 +0530

    Neon db

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,24 +1,53 @@
-import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
+// import { PrismaClient } from '@prisma/client';
+// import { PrismaPg } from '@prisma/adapter-pg';
+// import { Pool } from 'pg';
+
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+// });
+
+// const adapter = new PrismaPg(pool);
+
+// const globalForPrisma = globalThis as unknown as {
+//     prisma: PrismaClient | undefined;
+// };
+
+// export const prisma =
+//     globalForPrisma.prisma ??
+//     new PrismaClient({
+//         adapter,
+//         log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+//     });
+
+// if (process.env.NODE_ENV !== 'production') {
+//     globalForPrisma.prisma = prisma;
+// }
+
+
+import { PrismaClient } from '@prisma/client'
+import { PrismaNeon } from '@prisma/adapter-neon'
+import { Pool } from '@neondatabase/serverless'
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-});
+  connectionString: process.env.DATABASE_URL,
+})
 
-const adapter = new PrismaPg(pool);
+const adapter = new PrismaNeon(pool)
 
 const globalForPrisma = globalThis as unknown as {
-    prisma: PrismaClient | undefined;
-};
+  prisma: PrismaClient | undefined
+}
 
 export const prisma =
-    globalForPrisma.prisma ??
-    new PrismaClient({
-        adapter,
-        log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-    });
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['query', 'error', 'warn']
+        : ['error'],
+  })
 
 if (process.env.NODE_ENV !== 'production') {
-    globalForPrisma.prisma = prisma;
+  globalForPrisma.prisma = prisma
 }
