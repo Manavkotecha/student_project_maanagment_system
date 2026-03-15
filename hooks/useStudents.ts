63d4b3e25634bd0733ed37bced8658d55002048c
Manavkotecha <manavkotecha45@gmail.com>
2026-03-16 00:26:19 +0530

    Vercel 2

--- hooks/useStudents.ts
+++ hooks/useStudents.ts
@@ -1,17 +1,13 @@
 'use client';
 
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import type { Student, CreateStudentInput, ProjectGroupMember } from '@/app/types';
+import type { Student, CreateStudentInput, ProjectGroupMember, ProjectGroup } from '@/app/types';
 
 const QUERY_KEY = 'students';
 
 interface StudentWithGroups extends Student {
     ProjectGroupMember?: Array<ProjectGroupMember & {
-        ProjectGroup: {
-            ProjectGroupID: number;
-            ProjectGroupName: string;
-            ProjectTitle: string;
-        };
+        ProjectGroup: Pick<ProjectGroup, 'ProjectGroupID' | 'ProjectGroupName' | 'ProjectTitle'>;
     }>;
 }
 
